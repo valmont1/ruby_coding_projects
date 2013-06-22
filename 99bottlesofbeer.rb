e561d612deb590011a82eaf2326fc2b7291a06ca
valmont1 <mrniccu@gmail.com>
2013-06-21 23:43:02 -0700

    99 Bottles of Beer

--- 99bottlesofbeer.rb
+++ 99bottlesofbeer.rb
@@ -1,17 +1,17 @@
 # 99 Bottles of Beer
 
 def bottles(of_beer, plural)
-  puts "#{of_beer} bottle#{plural} of beer on the wall, #{of_beer} bottle#{plural} of beer."
-  puts "Take one down and pass it around, #{of_beer - 1} bottle#{plural} of beer on the wall!"
-  puts " "
+  puts "#{of_beer} bottle#{plural} of beer on the wall, #{of_beer} bottle#{plural} of beer."
+  puts "Take one down and pass it around, #{of_beer - 1} bottle#{plural} of beer on the wall!"
+  puts " "
 end
  
 99.downto(0) do |left|
-  if left > 1
-    bottles(left, "s")
-  elsif left == 1
-    bottles(left, "")
-  else
-    puts "No more bottles of beer on the wall, no more bottles of beer.  Go to the store and buy some more, 99 bottles of beer on the wall!"
-  end
-end 
+  if left > 1
+    bottles(left, "s")
+  elsif left == 1
+    bottles(left, "")
+  else
+    puts "No more bottles of beer on the wall, no more bottles of beer.  Go to the store and buy some more, 99 bottles of beer on the wall!"
+  end
+end
